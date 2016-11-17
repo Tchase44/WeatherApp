970c405126341e37c9c5b9a030596f87c3f27776
Tchase44 <tchase4544@gmail.com>
2016-11-17 17:36:56 -0600

    Final Draft #1
    happy with the results, may revisit in the future with an API more
    geared toward snow

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 function getRequest(zippyDoDah){
     var startUrl = 'https://api.worldweatheronline.com/premium/v1/search.ashx?';
     var keykey = 'key=cb4a3d52f4b54aa1993205712161011&q=';
-    var endingUrl = '&format=json&num_of_results=3&wct=Ski';
+    var endingUrl = '&format=json&num_of_results=2&wct=Ski';
 	var findResort = startUrl+keykey+zippyDoDah+endingUrl;
 
 	$.getJSON(findResort,function(datas){
@@ -20,95 +20,44 @@ function getRequest(zippyDoDah){
 		var long = datas.search_api.result[0].longitude;
 		var baseURL = 'https://api.worldweatheronline.com/premium/v1/ski.ashx?key=cb4a3d52f4b54aa1993205712161011&q=';
 		var locale = lat+','+long;
-		var endURL = '&format=json&date=today';
-		var input = baseURL+locale+endURL;
+		var endURLtoday = '&format=json&date=today';
+		var endURLtomorrow = '&format=json&date=tomorrow';
+		var inputToday = baseURL+locale+endURLtoday;
+		var inputTomorrow = baseURL+locale+endURLtomorrow;
 		$('p#resortName').text(datas.search_api.result[0].areaName[0].value+','+datas.search_api.result[0].region[0].value);
-		$.getJSON(input,function(datum){
+		$.getJSON(inputToday,function(datum){
 			var isItSnowing = datum.data.weather[0].chanceofsnow;
 			$('span#output').text(isItSnowing);
+		});
+		$.getJSON(inputTomorrow,function(datum){
+			var isItSnowing2 = datum.data.weather[0].chanceofsnow;
+			$('span#output2').text(isItSnowing2);
+		});
+	});
+	$.getJSON(findResort,function(datas){
+		var lat = datas.search_api.result[1].latitude;
+		var long = datas.search_api.result[1].longitude;
+		var baseURL = 'https://api.worldweatheronline.com/premium/v1/ski.ashx?key=cb4a3d52f4b54aa1993205712161011&q=';
+		var locale = lat+','+long;
+		var endURLtoday = '&format=json&date=today';
+		var endURLtomorrow = '&format=json&date=tomorrow';
+		var inputToday = baseURL+locale+endURLtoday;
+		var inputTomorrow = baseURL+locale+endURLtomorrow;
+		$('p#otherResort').text(datas.search_api.result[1].areaName[0].value+','+datas.search_api.result[1].region[0].value);
+		$.getJSON(inputToday,function(datum){
+			var isItSnowing3 = datum.data.weather[0].chanceofsnow;
+			$('span#output3').text(isItSnowing3);
+		});
+		$.getJSON(inputTomorrow,function(datum){
+			var isItSnowing4 = datum.data.weather[0].chanceofsnow;
+			$('span#output4').text(isItSnowing4);
 		});
 	});
 };
 
 
-var locale = {
-    "search_api": {
-        "result": [
-            {
-                "areaName": [
-                    {
-                        "value": "Liberty Mountain"
-                    }
-                ],
-                "country": [
-                    {
-                        "value": "United States of America"
-                    }
-                ],
-                "region": [
-                    {
-                        "value": "Pennsylvania"
-                    }
-                ],
-                "latitude": "39.750",
-                "longitude": "-77.375",
-                "population": "5000",
-                "weatherUrl": [
-                    {
-                        "value": "http://api.worldweatheronline.com/ski/v2/weather.aspx?q=39.7503,-77.3753"
-                    }
-                ]
-            },
-            {
-                "areaName": [
-                    {
-                        "value": "Eagle Rock"
-                    }
-                ],
-                "country": [
-                    {
-                        "value": "United States of America"
-                    }
-                ],
-                "region": [
-                    {
-                        "value": "Pennsylvania"
-                    }
-                ],
-                "latitude": "39.938",
-                "longitude": "-77.534",
-                "population": "5000",
-                "weatherUrl": [
-                    {
-                        "value": "http://api.worldweatheronline.com/ski/v2/weather.aspx?q=39.9376,-77.5339"
-                    }
-                ]
-            },
-            {
-                "areaName": [
-                    {
-                        "value": "Ski Roundtop"
-                    }
-                ],
-                "country": [
-                    {
-                        "value": "United States of America"
-                    }
-                ],
-                "region": [
-                    {
-                        "value": "Pennsylvania"
-                    }
-                ],
-                "latitude": "40.107",
-                "longitude": "-76.927",
-                "population": "5000",
-                "weatherUrl": [
-                    {
-                        "value": "http://api.worldweatheronline.com/ski/v2/weather.aspx?q=40.1067,-76.9267"
-                    }
-                ]
-            }
-        ]
-    }
-};
+
+
+
+
+
